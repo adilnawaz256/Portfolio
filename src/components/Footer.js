c4b8488ef4ccaa88120afd485c810dd2af8a26d7
Adil Nawaz <adilnawaz256@gmail.com>
2023-01-19 11:22:28 +0530

    'addsomefunctionalitye'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,14 +6,14 @@ export default function Footer(){
             <div className='row'>
                 <div className='col-lg-12'>
                     <ul className='contact-social'>
-                        <li><a href='https://www.linkedin.com/in/adil-nawaz-331210176/' target='_blank'><i className='uil uil-linkedin'></i></a></li>
-                        <li><a href='https://github.com/adilnawaz256/' target='_blank'><i className='uil uil-github'></i></a></li>
-                        <li><a href='mailto:adilnawaz256@gmail.com' target='_blank'><i className='uil uil-envelope-info'></i></a></li>
+                        <li><a href='https://www.linkedin.com/in/adil-nawaz-331210176/' target='_blank' rel="noreferrer"><i className='uil uil-linkedin'></i></a></li>
+                        <li><a href='https://github.com/adilnawaz256/' target='_blank' rel="noreferrer"><i className='uil uil-github'></i></a></li>
+                        <li><a href='mailto:adilnawaz256@gmail.com' target='_blank' rel="noreferrer"><i className='uil uil-envelope-info'></i></a></li>
                     </ul>
                 </div>                   
             </div>
         </div>
-        <a href='https://wa.me/+919625534956/?text=Hello Adil 🤙' className='whatsapp-btn animate__animated animate__backInDown' target='_blank'>
+        <a href='https://wa.me/+919625534956/?text=Hello Adil 🤙' className='whatsapp-btn animate__animated animate__backInDown' target='_blank' rel="noreferrer">
             <i className='uil uil-whatsapp my-float'></i>
         </a>
     </footer>
