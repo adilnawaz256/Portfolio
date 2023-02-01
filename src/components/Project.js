f83e29a02d6eeb48f90b5db9e7f5e021ca3bb398
Adil Nawaz <adilnawaz256@gmail.com>
2023-02-01 19:00:37 +0530

    'addsomefunctionality''

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,47 +1,51 @@
-import weather from '../img/weather.png'
-import dic from '../img/dictionary.png'
-import sc from '../img/sanpchart.png'
+import url from "./Projecturl.json";
 export default function Project() {
-  const mystyle ={
-    backgroundColor:"#EDF2FC"
-  }
-    return (
-        <section className='project' id='project' style={mystyle}>
-        <div className='container'>
-            <div className='row'>
-                <div className='col-12' data-aos="fade-down">
-                    <h2 className='title'>Project</h2>
+  const mystyle = {
+    backgroundColor: "#EDF2FC",
+  };
+  return (
+    <section className="project" id="project" style={mystyle}>
+      <div className="container">
+        <div className="row">
+          <div className="col-12" data-aos="fade-down">
+            <h2 className="title">Project</h2>
+          </div>
+          <div className="row">
+            {url.map((items) => {
+              return (
+                <div
+                  className="col-12 col-lg-4"
+                  data-aos="fade-up"
+                  data-aos-duration="1000"
+                >
+                  <div className="card mt-20">
+                    <a href={items.projecturl} target="_blank"  rel="noreferrer">
+                    <img
+                      src={items.imageurl}
+                      alt={items.alt}
+                      className="card-img-top"
+                    />
+                    <div className="card-body">
+                      <button
+                        className="btn btn-primary"
+                        onClick={() =>
+                          window.open(
+                            items.projecturl
+                          )
+                        }
+                      >
+                        Github
+                      </button>
+                      
+                    </div>
+                    </a>
+                  </div>
                 </div>
-                <div className='row'>
-                <div className='col-12 col-lg-4' data-aos="fade-up" data-aos-duration="1000">
-                   <div  className='card mt-20'>
-                      <img src={weather} alt='Project' className='card-img-top'/>
-                      <div className="card-body">
-                        <button className='btn btn-primary' onClick={()=> window.open('https://github.com/adilnawaz256/WeatherApp.git')}>Github</button>
-                    </div>
-                    </div>
-                </div>
-                <div className='col-12 col-lg-4' data-aos="fade-up" data-aos-duration="1000">
-                   <div  className='card mt-20'>
-                      <img src={dic} alt='Project' className='card-img-top'/>
-                      <div className="card-body">
-                        <button className='btn btn-primary' onClick={()=> window.open('https://github.com/adilnawaz256/DictonaryWebsite.git')}>Github</button>
-                    </div>
-                    </div>
-                </div>
-                <div className='col-12 col-lg-4' data-aos="fade-up" data-aos-duration="1000">
-                   <div  className='card mt-20'>
-                      <img src={sc} alt='Project' className='card-img-top'/>
-                      <div className="card-body">
-                        <button className='btn btn-primary' onClick={()=> window.open('https://github.com/adilnawaz2/Day06_SnapChat.git')}>Github</button>
-                    </div>
-                    </div>
-                </div>
-            </div>
-            </div>
+              );
+            })}
+          </div>
         </div>
+      </div>
     </section>
-    )
+  );
 }
-  
- 
